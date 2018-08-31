9e3bef6c8fd7f1d6dbdbb9fbc550b049fcebf89c
Gerov, Michael <Michael.Gerov@firstdata.com>
2018-08-31 09:08:01 -0400

    filewriter class created

---
+++ src/main/java/org/mhgerov/filelogger/FileLoggerFileWriter.java
@@ -0,0 +1,13 @@
+package org.mhgerov.filelogger;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileLoggerFileWriter {
+
+	public void writeToFile(String msg) throws IOException{
+
+	}
+}
