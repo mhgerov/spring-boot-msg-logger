8967b030d6d6b7a7ec71a7587b781a57490799ce
Gerov, Michael <Michael.Gerov@firstdata.com>
2018-08-31 08:10:30 -0400

    spring init

---
+++ src/main/java/org/mhgerov/filelogger/FileloggerApplication.java
@@ -0,0 +1,12 @@
+package org.mhgerov.filelogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileloggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileloggerApplication.class, args);
+	}
+}
---
+++ src/test/java/org/mhgerov/filelogger/FileloggerApplicationTests.java
@@ -0,0 +1,16 @@
+package org.mhgerov.filelogger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FileloggerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
